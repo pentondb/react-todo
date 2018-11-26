ea96121504236a838444b880f884758d9b6fe851
pentondb <derek.penton@gmail.com>
2018-11-25 19:04:02 -0600

    added todolist and todo components

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,13 +1,38 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
-var ToDoApp = React.createClass({
+var TodoApp = React.createClass({
+    getInitialState: function () {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                },
+                {
+                    id: 2,
+                    text: 'Clean the yard'
+                },
+                {
+                    id: 3,
+                    text: 'Build storage building'
+                },
+                {
+                    id: 4,
+                    text: 'Move craft materials to storage building'
+                }
+            ]
+        };
+    },
     render: function () {
+        var { todos } = this.state;
+
         return (
             <div>
-                ToDoApp.jsx
+                <TodoList todos={todos} />
             </div>
         );
     }
 });
 
-module.exports = ToDoApp;
+module.exports = TodoApp;
