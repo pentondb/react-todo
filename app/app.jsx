b24de4be3c20f7145fee850c38e2bf8682a5e7bf
pentondb <derek.penton@gmail.com>
2019-01-13 20:08:32 -0600

    Add redux store (do-over)

--- app/app.jsx
+++ app/app.jsx
@@ -1,21 +1,29 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+var React = require("react");
+var ReactDOM = require("react-dom");
+var { Route, Router, IndexRoute, hashHistory } = require("react-router");
 
-var TodoApp = require('TodoApp');
+var TodoApp = require("TodoApp");
+
+var actions = require("actions");
+var store = require("configureStore").configure();
+
+store.subscribe(() => {
+  console.log("New state", store.getState());
+});
+
+store.dispatch(actions.addTodo("Clean the yard"));
+store.dispatch(actions.setSearchText("yard"));
+store.dispatch(actions.toggleShowCompleted());
 
 // Load foundation
 /* Do not need the require lines? after adding the sassLoader entry in webpack.config.js, lecture 8-84 */
 /*
 require('style!css!foundation-sites/dist/css/foundation.min.css');
 */
-require('style!css!foundation-sites/dist/css/foundation-float.min.css');
+require("style!css!foundation-sites/dist/css/foundation-float.min.css");
 $(document).foundation();
 
 // App CSS
-require('style!css!sass!applicationStyles');
+require("style!css!sass!applicationStyles");
 
-ReactDOM.render(
-  <TodoApp />,
-  document.getElementById('app')
-);
+ReactDOM.render(<TodoApp />, document.getElementById("app"));
